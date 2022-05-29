a918e2d31a38f7c35f543bc6f2b9b9c770610763
Dwight-Schrutee <89634780+Dwight-Schrutee@users.noreply.github.com>
2022-05-29 23:43:19 +0530

    Update index.js

--- MERN/src/index.js
+++ MERN/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import "./components/css/index.css";
 import App from './App';
 
-console.log("pearl is a better name than naman");
 ReactDOM.render(
   <React.StrictMode>
     <App />
